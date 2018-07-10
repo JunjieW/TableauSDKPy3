5dbf39f75935ebb490cc40331aba6b73eda6ec88
Junjie Wei <jwei@Bamfunds.com>
2018-07-10 12:18:29 -0500

    update unittest

--- test/test_create_open_write_extract.py
+++ test/test_create_open_write_extract.py
@@ -1,87 +1,91 @@
-import argparse
-import sys
-import textwrap
+import unittest
 
 from tableausdkpy3 import *
 from tableausdkpy3.Extract import *
-# from tableausdkpy3.Server import *
 
-# ------------------------------------------------------------------------------
-#   Create Extract
-# ------------------------------------------------------------------------------
-#   (NOTE: This function assumes that the Tableau SDK Extract API is initialized)
-def createOrOpenExtract(filename, useSpatial):
-    try:
-        # Create Extract Object
-        # (NOTE: The Extract constructor opens an existing extract with the
-        #  given filename if one exists or creates a new extract with the given
-        #  filename if one does not)
-        extract = Extract(filename)
 
-        # Define Table Schema (If we are creating a new extract)
-        # (NOTE: In Tableau Data Engine, all tables must be named 'Extract')
-        if (not extract.hasTable('Extract')):
-            schema = TableDefinition()
-            schema.setDefaultCollation(Collation.EN_GB)
-            schema.addColumn('Purchased', Type.DATETIME)
-            schema.addColumn('Product', Type.CHAR_STRING)
-            schema.addColumn('uProduct', Type.UNICODE_STRING)
-            schema.addColumn('Price', Type.DOUBLE)
-            schema.addColumn('Quantity', Type.INTEGER)
-            schema.addColumn('Taxed', Type.BOOLEAN)
-            schema.addColumn('Expiration Date', Type.DATE)
-            schema.addColumnWithCollation('Produkt', Type.CHAR_STRING, Collation.DE)
-            if (useSpatial):
+class ExtractAndTypesTestCase(unittest.TestCase):
+    def setUp(self):
+        # file path to output std
+        self.filename = 'test_out.tde'
+
+        # Initialize Tableau SDK Extract API
+        ExtractAPI.initialize()
+
+    def tearDown(self):
+        # Close the Tableau Extract API
+        ExtractAPI.cleanup()
+
+
+    def test_create_or_open_extract(self):
+        """
+        Create Extract (NOTE: This function assumes that the Tableau SDK Extract API is initialized)
+        """
+        try:
+            # Create Extract Object
+            # (NOTE: The Extract constructor opens an existing extract with the
+            #  given filename if one exists or creates a new extract with the given
+            #  filename if one does not)
+            extract = Extract(self.filename)
+
+            # Define Table Schema (If we are creating a new extract)
+            # (NOTE: In Tableau Data Engine, all tables must be named 'Extract')
+            if (not extract.hasTable('Extract')):
+                schema = TableDefinition()
+                schema.setDefaultCollation(Collation.EN_GB)
+                schema.addColumn('Purchased', Type.DATETIME)
+                schema.addColumn('Product', Type.CHAR_STRING)
+                schema.addColumn('uProduct', Type.UNICODE_STRING)
+                schema.addColumn('Price', Type.DOUBLE)
+                schema.addColumn('Quantity', Type.INTEGER)
+                schema.addColumn('Taxed', Type.BOOLEAN)
+                schema.addColumn('Expiration Date', Type.DATE)
+                schema.addColumnWithCollation('Produkt', Type.CHAR_STRING, Collation.DE)
                 schema.addColumn('Destination', Type.SPATIAL)
-            table = extract.addTable('Extract', schema)
-            if (table == None):
-                print('A fatal error occurred while creating the table:\nExiting now\n.')
-                exit(-1)
+                table = extract.addTable('Extract', schema)
+                if (table == None):
+                    raise Exception('A fatal error occurred while creating the table:\nExiting now\n.')
 
-    except TableauException as e:
-        print('A fatal error occurred while creating the new extract:\n', e, '\nExiting now.')
-        exit(-1)
+            extract.close()
 
-    return extract
+        except TableauException as e:
+            print('A fatal error occurred while creating the new extract:\n', e, '\nExiting now.')
+            exit(-1)
 
 
-def populateExtract(extract, useSpatial):
-    try:
-        # Get Schema
-        table = extract.openTable('Extract')
-        schema = table.getTableDefinition()
 
-        # Insert Data
-        row = Row(schema)
-        row.setDateTime(0, 2012, 7, 3, 11, 40, 12, 4550)  # Purchased
-        row.setCharString(1, 'Beans')  # Product
-        row.setString(2, u'uniBeans')  # Unicode Product
-        row.setDouble(3, 1.08)  # Price
-        row.setDate(6, 2029, 1, 1)  # Expiration Date
-        row.setCharString(7, 'Bohnen')  # Produkt
-        for i in range(10):
-            row.setInteger(4, i * 10)  # Quantity
-            row.setBoolean(5, i % 2 == 1)  # Taxed
-            if useSpatial:
+
+    def test_populateExtract(self):
+        """
+        Test creating all available types in an Tableau Extract, if None, default will be used
+        """
+        try:
+            extract = Extract(self.filename)
+
+            # Get Schema
+            table = extract.openTable('Extract')
+            schema = table.getTableDefinition()
+
+            # Insert Data
+            row = Row(schema)
+            row.setDateTime(0, 2012, 7, 3, 11, 40, 12, 4550)  # Purchased
+            row.setCharString(1, 'Beans')  # Product
+            row.setString(2, u'uniBeans')  # Unicode Product
+            row.setDouble(3, 1.08)  # Price
+            row.setDate(6, 2029, 1, 1)  # Expiration Date
+            row.setCharString(7, 'Bohnen')  # Produkt
+            for i in range(10):
+                row.setInteger(4, i * 10)  # Quantity
+                row.setBoolean(5, i % 2 == 1)  # Taxed
                 row.setSpatial(8, "POINT (30 10)")  # Destination
-            table.insert(row)
+                table.insert(row)
 
-    except TableauException as e:
-        print('A fatal error occurred while populating the extract:\n', e, '\nExiting now.')
-        exit(-1)
+            extract.close()
 
+        except TableauException as e:
+            print('A fatal error occurred while populating the extract:\n', e, '\nExiting now.')
+            exit(-1)
 
 
 if __name__ == '__main__':
-    ExtractAPI.initialize()
-    filename = 'test_out.tde'
-    # Create or Expand the Extract
-    extract = createOrOpenExtract(filename, True)
-    populateExtract(extract, True)
-
-    # Flush the Extract to Disk
-    extract.close()
-
-    # Close the Tableau Extract API
-    ExtractAPI.cleanup()
-
+    unittest.main()
